503ecfe7477bf99f405a8eda69b0348fcce8f581
Shakib-Shajid <shakibalshajid1682@gmail.com>
2024-06-26 21:37:14 +0600

    access form info

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,21 +1,32 @@
 import { Link } from "react-router-dom";
 
 const Login = () => {
+
+
+    // access the form info
+    const handleLogin = event =>{
+        event.preventDefault();
+        const form = event.target;
+        const email = form.email.value;
+        const password = form.password.value;
+
+        console.log(email, password);
+    }
     return (
         <div>
-            <form className="card-body md:w-2/3 mx-auto">
+            <form onSubmit = {handleLogin} className="card-body md:w-2/3 mx-auto">
                 <h3 className="text-center font-bold text-xl md:text-3xl">Login Page</h3>
                 <div className="form-control">
                     <label className="label">
                         <span className="label-text">Email</span>
                     </label>
-                    <input type="email" placeholder="email" className="input input-bordered" required />
+                    <input type="email" name="email" placeholder="email" className="input input-bordered" required />
                 </div>
                 <div className="form-control">
                     <label className="label">
                         <span className="label-text">Password</span>
                     </label>
-                    <input type="password" placeholder="password" className="input input-bordered" required />
+                    <input type="password" name="password" placeholder="password" className="input input-bordered" required />
                     <label className="label">
                         <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
                     </label>
